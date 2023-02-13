d22c7813f0e8f975df540fb75293e9931dcc772a
Lam Quang Khai <khailq@devcamp.edu.vn>
2023-02-13 21:10:00 +0700

    quen

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(cors());
 
 
 //Ket noi mongo
-mongoose.connect('mongodb://127.0.0.1:27017/CRUD_Shop24h', function (error) {
+mongoose.connect('mongodb://mongo:MiMyogY3UnecNs9c1guT@containers-us-west-99.railway.app:7993', function (error) {
     if (error) throw error;
     console.log('Successfully connected MongoDB');
 })
